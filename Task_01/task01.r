16b1a0373fc48718ddbba5075a513b231d1931e2
nicoledk19 <ndk0008@mix.wvu.edu>
2023-01-11 10:42:55 -0500

    NicoleKester Task 01 upload a

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -26,3 +26,59 @@ my_div
 2
 2
 1
+library(swirl)
+swirl()
+Nicole Kester
+1
+2
+getwd()
+ls()
+x<-9
+ls()
+list.files()
+?list.files
+args()
+args(list.files())
+args(list.files)
+old.dir<-list.files
+old.dir<-getwd()
+testdir<-dir.create()
+dir.create()
+dir.create(testdir)
+testdir<-dir.create(old.dir)
+dir.create("testdir")
+setwd("testdir")
+file.create("mytest.R")
+ls()
+list.files()
+file.exists("mytest.R")
+file.info("mytest.R")
+file.rename("mytest2.R")
+file.rename("mytest.R, mytest2.R")
+file.rename()
+file.rename(mytest.R)
+file.rename(mytest.R, mytest2.R)
+file.rename("mytest.R", "mytest2.R")
+file.copy("mytest2.R", "mytest3.R")
+file.path("mytest3.R")
+file.path("folder1", "folder2")
+?dir.create
+dir.create(file.path("testdir2", "testdir3"), recursive=TRUE)
+setwd(old.dir)
+2
+1
+12
+ls()
+class(plants)
+dim(plants)
+nrow(plants)
+ncol(plants)
+object.size(plants)
+names(plants)
+head(plants)
+head(plants, 10)
+tail(plants, 15)
+summary(plants)
+table(plants$Active_Growth_Period)
+str(plants)
+2
