cd424487f0764f6a9309258f6c0f77723d3d759e
nicoledk19 <ndk0008@mix.wvu.edu>
2023-03-01 12:21:52 -0500

    Task 06

--- Task_06/task.06.R
+++ Task_06/task.06.R
@@ -32,7 +32,7 @@ dev.off()
 
 pdf("006_plot_n190h1s0.pdf")
 smoothScatter(dYear, dAlleles, colramp=Pal, nbin=100, xlab="year", ylab="change in allele freq. since 1998")
-addFit(nruns=50, n=190, ngens=18, startT=1997, simCol="black", rescale=TRUE, s=0, h=1)
+addFit(nruns=100, n=190, ngens=18, startT=1997, simCol="black", rescale=TRUE, s=0, h=1)
 #if n is 200 then most of the time the yellow at the beginning is not covered, so decreasing it causes more spikes overall but has more of the yellow covered, dropping to 175 seems to be the best option because most of them stay inside the purple while still spanning the whole possiblity of distributions. if selection is positive it spikes upward quickly and if negative it spikes quickly downward so it fits best at zero, if s is zero then h will always have the same effect so it does not matter what you set h to. this means that this population did not experience a lot of selection and it likley had a population size of around 150-200. selection =0 is better to get line in middle. s=1 it skyrockets. h=about the same when .25 and .75, .5 has some variety. h=0.75, black
 dev.off()
 
